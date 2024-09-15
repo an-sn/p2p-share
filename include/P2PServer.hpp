4d5f357f78046150a6a8df13909fcb5833efa7f8
Aditya Narayan <adi7.narayan@gmail.com>
2024-09-16 00:17:31 +0530

    Basic handling of HTTP requests

--- include/P2PServer.hpp
+++ include/P2PServer.hpp
@@ -5,16 +5,15 @@
 
 constexpr int PORT_NUMBER = 8080;
 
-class P2PServer
-{
-    public:
-        P2PServer();
-        ~P2PServer();
-        void startListening();
-        bool connectToDatabase(std::string ipAddress, unsigned short port);
+class P2PServer {
+  public:
+    P2PServer();
+    ~P2PServer();
+    void startListening();
+    bool connectToDatabase(std::string ipAddress, unsigned short port);
 
-    private:
-        net::io_context  m_ioc;
-        tcp::acceptor    m_acceptor;
-        RedisPeerStorage m_redisDb;
+  private:
+    net::io_context m_ioc;
+    tcp::acceptor m_acceptor;
+    RedisPeerStorage m_redisDb;
 };
