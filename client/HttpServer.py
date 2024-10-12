87f1e75b1895690cba54ae1e6242f1ecff6bf39f
Aditya Narayan <adi7.narayan@gmail.com>
2024-10-12 16:37:57 +0530

    Added code to handle file upload and download for peers

---
+++ client/HttpServer.py
@@ -0,0 +1,51 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import threading
+import logging
+import os
+
+logging.getLogger("http.server").setLevel(logging.ERROR)
+
+class HttpFileHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, available_files={}, **kwargs):
+        self.available_files = available_files
+        super().__init__(*args, **kwargs)
+    def do_GET(self):
+        if self.headers['Content-Type'] != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+        try:
+            content_length = int(self.headers['Content-Length'])
+            request_body = self.rfile.read(content_length)
+            json_data = json.loads(request_body)
+            (file_uuid, chunk_id) = (json_data["file_uuid"], json_data["chunk_id"])
+            chunk_path = os.path.join("/tmp/p2p_client_data", file_uuid, "chunks/", f"chunk{str(chunk_id)}")
+            if not os.path.isfile(chunk_path):
+                self.send_response(404)
+                self.end_headers()
+                return
+            with open(chunk_path, 'rb') as chunk_file:
+                chunk_data = chunk_file.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/octet-stream')
+            self.send_header('Content-Length', str(len(chunk_data)))
+            self.end_headers()
+            self.wfile.write(chunk_data)
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+
+def run_http_file_server(ip='', port=9192, available_files={}):
+    def handler(*args, **kwargs):
+        HttpFileHandler(*args, available_files=available_files, **kwargs)
+    server_address = (ip, port)
+    httpd = HTTPServer(server_address, handler)
+    httpd.serve_forever()
+
+def start_http_server_thread(ip='', port=9192, available_files={}):
+    server_thread = threading.Thread(target=run_http_file_server, args=(ip, port, available_files))
+    server_thread.daemon = True
+    server_thread.start()
+    return server_thread
--- client/client.py
+++ client/client.py
@@ -1,15 +1,17 @@
+import argparse
 import sys
-import requests
-import json
-import socket
 import os
+import threading
+import requests
+import uuid
 import shutil
 import hashlib
-import uuid
+from HttpServer import *
 
 CHUNK_SIZE = 1 * 1024 * 1024
 peer_uuid = None
 advertised_files = {}
+available_files = {}
 
 def validate_file(file_name):
     if not os.path.isfile(file_name):
@@ -17,10 +19,8 @@ def validate_file(file_name):
         return False
     return True
 
-def chunk_file(file_name, chunk_dir):
-    base_name = os.path.basename(file_name)
-    if not os.path.exists(chunk_dir):
-        os.makedirs(chunk_dir)
+def chunk_file(file_name, chunks_path):
+    # base_name = os.path.basename(file_name)
     chunk_hashes = []
     chunk_index = 0
     with open(file_name, 'rb') as f:
@@ -28,7 +28,7 @@ def chunk_file(file_name, chunk_dir):
             chunk_data = f.read(CHUNK_SIZE)
             if not chunk_data:
                 break
-            chunk_file_name = os.path.join(chunk_dir, f"{base_name}_chunk_{chunk_index}")
+            chunk_file_name = os.path.join(chunks_path, f"chunk{chunk_index}")
             with open(chunk_file_name, 'wb') as chunk_file:
                 chunk_file.write(chunk_data)
             chunk_hash = hashlib.sha256(chunk_data).hexdigest()
@@ -36,29 +36,130 @@ def chunk_file(file_name, chunk_dir):
             chunk_index += 1
     return chunk_hashes
 
-def file_list():
-    pass
+def print_available_files():
+    if not available_files:
+        print("No files available.")
+        return
+    print("\nAvailable Files:")
+    print("-" * 50)
+    for file_uuid, info in available_files.items():
+        print(f"File name: {info["file_name"]}")
+        print(f"  UUID: {file_uuid}")
+        print(f"  Description: {info['file_description']}")
+        print(f"  Size: {info['file_size']} bytes")
+        print("-" * 50)
 
-def file_request():
-    pass
+def file_list(print = False):
+    try:
+        header = {'Content-Type': 'application/json'}
+        response = requests.get(f"{server_url}/file_list", headers=header)
+        response.raise_for_status()
+        response_json = response.json()
+        for file_uuid, file_metadata in response_json.items():
+            file_info = {
+                "file_name" : file_metadata['file_name'],
+                "file_description" : file_metadata['file_description'],
+                "file_size" : file_metadata['file_size']
+            }
+            available_files[file_uuid] = file_info
+        if print:
+            print_available_files()
+        return response_json
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send file list request, exception: {e}")
+        return None
+
+def download_chunk(file_uuid, chunk_index, chunk_info, download_path):
+    peers = chunk_info["peers"]
+    (peer_ip, peer_port) = (peers[0]["ip"], peers[0]["port"])
+    peer_url = "http://" + peer_ip + ":" + peer_port
+    print(peer_url)
+    print(f"Downloading file:{file_uuid}:chunk:{chunk_index} from peer: ({peer_ip}, {peer_port})")
+    try:
+        chunk_request = {
+            "file_uuid" : file_uuid,
+            "chunk_id" : chunk_index
+        }
+        json_req = json.dumps(chunk_request)
+        header = {'Content-Type': 'application/json', 'Content-Length': str(len(json_req))}
+        response = requests.get(f"{peer_url}/chunk_request", headers=header, data=json_req)
+        response.raise_for_status()
+        chunk_dir = os.path.join(download_path, "chunks")
+        if not os.path.exists(chunk_dir):
+            os.makedirs(chunk_dir)
+        chunk_file_path = os.path.join(chunk_dir, f"chunk_{chunk_index}")
+        sha256_hash = hashlib.sha256()
+        with open(chunk_file_path, 'wb') as chunk_file:
+            for chunk in response.iter_content(chunk_size=4096):
+                if chunk:  # Filter out keep-alive new chunks
+                    chunk_file.write(chunk)
+                    sha256_hash.update(chunk)
+        downloaded_chunk_hash = sha256_hash.hexdigest()
+        if(downloaded_chunk_hash == chunk_info["hash"]):
+            return True
+        else:
+            print(f"Mismatch in chunk hash, expected: {chunk_info["hash"]}, Actual : f{downloaded_chunk_hash}")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to get file:{file_uuid}::chunk:{chunk_index}, exception: {e}")
+        return False
+
+def reassemble_chunks(file_name, download_path, number_of_chunks):
+    chunk_dir = os.path.join(download_path, "chunks")
+    target_file_path = os.path.join(download_path, file_name)
+    with open(target_file_path, 'wb') as f:
+        for chunk_index in range(number_of_chunks):
+            chunk_file_path = os.path.join(chunk_dir, f"chunk_{chunk_index}")
+            with open(chunk_file_path, 'rb') as chunk_file:
+                chunk_data = chunk_file.read()
+                f.write(chunk_data)
+
+def download_file(file_uuid, file_details, download_path):
+    chunk_index = 0
+    chunk_list = file_details["chunks"]
+    entire_file_downloaded = True
+    missing_chunks = []
+    for chunk_info in chunk_list:
+        chunk_downloaded = download_chunk(file_uuid, chunk_index, chunk_info, download_path)
+        if not chunk_downloaded:
+            entire_file_downloaded = False
+            missing_chunks.append(chunk_index)
+        chunk_index += 1
+    if entire_file_downloaded:
+        reassemble_chunks(available_files[file_uuid]["file_name"], download_path, len(chunk_list))
+
+# HTTP requests
+
+def file_request(file_uuid):
+    if file_uuid not in available_files:
+        print("Entered file UUID is not valid!")
+        return
+    download_path = f"/tmp/p2p_client/{file_uuid}/"
+    try:
+        header = {'Content-Type': 'application/json'}
+        file_request_msg = {
+            "file_uuid" : file_uuid
+        }
+        response = requests.get(f"{server_url}/file_request", headers=header, data=json.dumps(file_request_msg))
+        response.raise_for_status()
+        download_file(file_uuid, response.json(), download_path)
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send the file request, exception: {e}")
 
 def file_advert(file_name, chunk_size):
     if not validate_file(file_name):
         return
-    file_dir_name = os.path.dirname(file_name)
-    base_name = os.path.basename(file_name)
-    temp_dir = os.path.join(file_dir_name, "file_copy")
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-    destination_file_path = os.path.join(temp_dir, base_name)
-    shutil.copy2(file_name, destination_file_path)
-    chunks_path = os.path.join(temp_dir, "chunks")
-    chunk_hashes = chunk_file(destination_file_path, chunks_path)
+    file_uuid = str(uuid.uuid4())
+    destination_path = os.path.join("/tmp/p2p_client_data/", file_uuid)
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    chunks_path = os.path.join(destination_path, "chunks")
+    if not os.path.exists(chunks_path):
+        os.makedirs(chunks_path)
+    chunk_hashes = chunk_file(file_name, chunks_path)
     try:
-        file_uuid = str(uuid.uuid4())
         file_advert_msg = {
             "peer_uuid": peer_uuid,
-            "file_name": base_name,
+            "file_name": os.path.basename(file_name),
             "file_uuid": file_uuid,
             "file_size": os.path.getsize(file_name),
             "file_description": "Hello",
@@ -68,18 +169,18 @@ def file_advert(file_name, chunk_size):
         header = {'Content-Type': 'application/json'}
         response = requests.post(f"{server_url}/file_advert", headers=header, data=json.dumps(file_advert_msg))
         response.raise_for_status()
-        advertised_files[file_uuid] = {"path" : destination_file_path, "chunk_dir": chunks_path}
+        advertised_files[file_uuid] = {"path" : destination_path, "chunk_dir": chunks_path, "chunk_hashes" : chunk_hashes}
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Failed to send file request, exception: {e}")
         return None
 
-def connect_to_server(server_url):
+def connect_to_server(server_url, client_ip, client_port):
     header = {'Content-Type': 'application/json'}
     try:
         discovery_request = {
-            "peer_ip": "192.168.0.190",
-            "peer_port": 1234
+            "peer_ip": client_ip,
+            "peer_port": client_port
         }
         response = requests.post(f"{server_url}/discovery", headers=header, data=json.dumps(discovery_request))
         response.raise_for_status()
@@ -87,6 +188,8 @@ def connect_to_server(server_url):
     except requests.exceptions.RequestException as e:
         print(f"Failed to connect to server, exception: {e}")
         return None
+
+# Command handling
 
 commands = {
     "file_advert": {
@@ -98,7 +201,7 @@ commands = {
         "description": "Get a list of files that the server is currently aware of."
     },
     "file_request": {
-        "usage": "file_request <file_name>",
+        "usage": "file_request <file_uuid>",
         "description": "Get file details from server and download if all chunks can be found."
     }
 }
@@ -123,12 +226,26 @@ def wait_for_command():
                 file_advert(command_parts[1].strip(), 1024*1024)
             else:
                 print(commands["file_advert"]["usage"])
+        elif command_type == "file_list":
+            file_list(True)
+        elif command_type == "file_request":
+            if(len(command_parts) == 2):
+                file_request(command_parts[1].strip())
+
+# Main function
 
 if __name__ == "__main__":
-    server_url = "http://192.168.0.190:5100"
-    discovery_response = connect_to_server(server_url)
+    parser = argparse.ArgumentParser(description="P2P file transfer client")
+    parser.add_argument('--server_ip', type=str, required=True, help="IP address of p2p tracker")
+    parser.add_argument('--server_port', type=int, required=True, help="p2p tracker's listening port")
+    parser.add_argument('--client_ip', type=str, required=True, help="IP address of host machine.")
+    parser.add_argument('--client_port', type=int, required=True, help="Port to which client will bind to communicate with peers")
+    args = parser.parse_args()
+    server_url = f"http://{args.server_ip}:{args.server_port}"
+    discovery_response = connect_to_server(server_url, args.client_ip, args.client_port)
     if discovery_response == None:
         sys.exit(1)
     peer_uuid = discovery_response['uuid']
-    print(f"\nConnected to server!")
+    file_list()
+    start_http_server_thread(args.client_ip, args.client_port, available_files)
     wait_for_command()
